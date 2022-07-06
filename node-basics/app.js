24fd258e076b279cf66c7a926952b4cf93ff05ad
KOBEsphinx242 <ado.coneo@gmail.com>
2022-07-05 21:40:29 -0500

    node basics

--- node-basics/app.js
+++ node-basics/app.js
@@ -1,7 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const { method, url } = req;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST">');
+    res.write('<input type="text" name="message" />');
+    res.write('<button type="submit">Send</button>');
+    res.write('</form></body>');
+    res.write('</html>');
+    return res.end();
+  } else if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.writeHead(302, { Location: '/' });
+    return res.end();
+  }
+  //process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<head><title>Enter Message</title></head>');
+  res.write('<body><h1>JHllo World</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
